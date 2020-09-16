ad29adee39e6687afff42ce272197fe38720b6ee
LT Baby <trevor998tredoux@gmail.com>
2020-09-17 01:02:21 +0200

    v1.0_20200917

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuDisplay(theHamburger) {
+    theHamburger.classList.toggle("pattyRotate");
+    var element = document.getElementById("thesidebar");
+    element.classList.toggle("sideNavAnim");
+    //document.getElementById("sidebarnav").style.width = "0px";
+  }
+ 
